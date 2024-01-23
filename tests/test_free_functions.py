ad236b3b466e642bb37674336cdd85947e8e3559
m0bb1n <heyawmeteke@gmail.com>
2024-01-22 19:20:03 -0800

    added integration testing

--- tests/test_free_functions.py
+++ tests/test_free_functions.py
@@ -1,8 +1,59 @@
 import pytest
 from gofilepy import GofileClient
 
-client = GofileClient()
+#integration testing for free features
 
-def test_uploading_file():
-    pass
+FILE_PATH = __file__ #uploads script file for test 
+STANDARD_TOKEN = "" #get token in dashboard at gofile.io/myProfile
 
+guest_client = GofileClient()
+standard_client = GofileClient(token=STANDARD_TOKEN, get_account=False) #get free token from dashboard
+
+def assert_GofileFile(f):
+    assert type(f.parent_id) == str
+    assert type(f.page_link) == str
+    assert type(f.name) == str
+    assert type(f.md5) == str
+    f.delete()
+    assert f.is_deleted == True
+
+def test_file_upload_standard():
+   f = standard_client.upload(FILE_PATH, parent_id=standard_client.account.root_id)
+   assert_GofileFile(f)
+
+def test_file_upload_guest():
+    f = guest_client.upload(FILE_PATH)
+    assert_GofileFile(f)
+
+def test_account_get():
+    account = standard_client.get_account()
+
+    assert account.tier == "standard"
+    assert type(account.total_size) == int
+    assert type(account.root_id) == str
+    assert type(account.email) == str
+    assert type(account.token) == str
+    return account
+
+def test_folder_create():
+    name = "test_folder"
+    folder = standard_client.create_folder(name, standard_client.account.root_id)
+
+    assert folder.name == name
+    assert folder.parent_id == standard_client.account.root_id
+    return folder
+
+
+def test_content_delete(c):
+    assert c.is_deleted == False
+    c.delete()
+    assert c.is_deleted == True
+
+if __name__ == "__main__":
+    test_file_upload_guest()
+
+    test_account_get()
+    test_file_upload_standard()
+    folder = test_folder_create()
+
+    test_content_delete(folder)
--- tests/test_premium_functions.py
+++ tests/test_premium_functions.py
@@ -1,5 +1,99 @@
 import pytest
+import os
+from time import sleep, time
 from gofilepy import GofileClient
 
-client = GofileClient(token="")
+#integration testing for premium features
+
+FILE_PATH = __file__ #uploads script file for test 
+PREMIUM_TOKEN = "" #get token in dashboard at gofile.io/myProfile (paid user)
+
+client = GofileClient(token=PREMIUM_TOKEN, get_account=False)
+
+def test_file_upload(parent_id):
+    f = client.upload(FILE_PATH, parent_id=parent_id)
+    assert type(f.parent_id) == str
+    assert type(f.page_link) == str
+    assert type(f.name) == str
+    assert type(f.md5) == str
+    return f
+
+
+def test_account_get():
+    account = client.get_account()
+
+    assert account.tier == "premium"
+    assert type(account.total_size) == int
+    assert type(account.root_id) == str
+    assert type(account.email) == str
+    assert type(account.token) == str
+
+def test_content_copy(c):
+    #copyContent api doesn't return any data so assertion reload parent folder to see an increase in children
+    folder = client.get(c.parent_id)
+    num_of_children = len(folder.children)
+
+    c.copy_to(c.parent_id)
+    sleep(9) #copyContent endpoint seems to take the longest to register changes
+    folder.reload()
+
+    assert len(folder.children) == num_of_children + 1
+
+def test_file_set_options(f):
+    f.set_option("directLink", True)
+    sleep(5)
+    f.reload()
+
+    assert type(f.direct_link) == str
+
+def test_folder_set_options(folder):
+    desc = "A short description"
+    folder.set_option("public", False)
+    folder.set_option("password", "secret")
+    folder.set_option("description", desc)
+    folder.set_option("tags", ["test","gofilepy"])
+    folder.set_option("expire", int(time()+120)) #Expires in 2 mins
+    sleep(5)
+
+    folder.reload()
+
+    assert folder.has_password == True
+    assert folder.is_public == False
+    assert folder.description == desc 
+
+def test_file_download(f):
+    path =  f.download()
+    data_download = None
+    data_local = None
+
+    with open(FILE_PATH, "rb") as f_local:
+        data_local = f_local.read()
+    with open(path, "rb") as f_download:
+        data_download = f_download.read()
+
+    assert data_local == data_download
+    os.remove(path) #remove downloaded test file
+
+def test_folder_create():
+    name = "test_folder"
+    folder = client.create_folder(name, client.account.root_id)
+
+    assert folder.name == name
+    assert folder.parent_id == client.account.root_id
+    return folder
+
+
+if __name__ == "__main__":
+    test_account_get()
+
+    folder = test_folder_create()
+    test_folder_set_options(folder)
+
+    f = test_file_upload(folder.content_id)
+    test_file_set_options(f)
+    test_content_copy(f)
+    test_file_download(f)
+
+    test_content_delete(f)
+    test_content_delete(folder)
 
